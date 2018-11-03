64b99eaf1662eef0ea93e76dc9182d14f6dedb3b
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 14:35:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment.rb'
 
 class Song
   extend Memorable
-  extend Findable
+  # extend Findable
 
   attr_accessor :name
   attr_reader :artist
@@ -14,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
 
   def self.all
